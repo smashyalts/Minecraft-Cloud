1e60fba4173be1e4b9b218ddb5e14b05c77d28c8
smashyalts <smashyalts@gmail.com>
2023-03-20 11:12:50 +0100

    Update StartServer.py

--- StartServer.py
+++ StartServer.py
@@ -3,5 +3,5 @@ public_ip = input("Input the ip of ur previously made server: ")
 password = input("Input the password of ur previously made server: ")
 script = "ssh " + "ubuntu@" + public_ip + " -p " + password
 os.system(script)
-startserver = "sudo screen java -Xmx1500MB -jar server.jar"
+startserver = "sudo screen java -Xmx1000MB -jar server.jar"
 os.system(startserver)
